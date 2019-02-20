c76f0b0d51e53fce03efc7fdc8d4d427d2c20046
bharath993 <bharath993@gmail.com>
2019-02-20 14:13:37 +0530

    changes made as before

--- NVGRE_VXLAN/Chelsio_Configure_Nvgre_Vxlan.ps1
+++ NVGRE_VXLAN/Chelsio_Configure_Nvgre_Vxlan.ps1
@@ -23,9 +23,26 @@ exit
 }
 
 
+
 Write-Host "starting the WINRM service" -f Yellow
 Start-Service WinRM
 Set-Item WSMan:\localhost\Client\TrustedHosts * -Force
+
+
+function basic_setup{
+
+if (!(Test-Path ".\configure_start_vmnetworkadapter.ps1")){
+write-host ".\configure_start_vmnetworkadapter.ps1 is missing!!!,it must be present in the same location as this script" -f Yellow
+exit
+}
+else {
+Invoke-Expression -Command .\configure_start_vmnetworkadapter.ps1
+Copy-Item .\configure_start_vmnetworkadapter.ps1 \\$remote_ip\C$\Test -Force
+Invoke-Command -ComputerName $remote_ip -ScriptBlock {Invoke-Expression -Command $args[0]} -ArgumentList C:\Test\.\configure_start_vmnetworkadapter.ps1
+sleep 3
+}
+}
+
 
 function remove_mac_file($content){
 
@@ -38,12 +55,21 @@ $a | Set-Content $content
 
 }
 
+
+function content_edit_vswitch($content){
+write-host "Editing the vswitch name" -f Yellow
+$a[7] = $a[7] | ForEach-Object {$_ -replace "\w+",""}
+$a[7] = $a[7] -replace "#`": `"`"","#VFPSWITCH`": `"p0`""
+$a | Set-Content $content
+}
+
 function content_edit($mac_list,$content,$judge){
- 
+
 $a = Get-Content $content
 $j = 0
 if($judge -eq 1)
 {
+write-host "Editing the mac-address" -f Yellow
 for($i = 1;$i -lt $no_of_vm ;$i = $i + 2 )
 {
 $a[6] = $a[6]  | ForEach-Object { $_ -replace "VM$($i)_MAC`": `"`"","VM$($i)_MAC`": `"$($mac_list[$j])`""}
@@ -51,18 +77,22 @@ $j++
 }
 }
 else {
+write-host "Editing the mac-address of remote machine" -f Yellow
 for($i = 2;$i -le $no_of_vm ; $i = $i + 2){
-
 $a[6] = $a[6]  | ForEach-Object { $_ -replace "VM$($i)_MAC`": `"`"","VM$($i)_MAC`": `"$($mac_list[$j])`""}
 $j++
 }
 }
 $a  | set-content -Path $content
 
+
+
 if($judge -ne 1)
 {
+content_edit_vswitch $content 
 Write-Host "Copying the $content to the Remote Machine:$remote_ip" -f Yellow
 Copy-Item $content \\$remote_ip\C$\Test -Force
+write-host "Copied!!" -f Green
 }
 
 }
@@ -98,6 +128,8 @@ $mac_list = insert $macaddress_remote
 
 content_edit $mac_list $content 2
 }
+
+
 
 
 function select_case
@@ -138,6 +170,6 @@ $content = "C:\Test\HNV_2host_8vms.json"
 return $content
 }
 
-
+basic_setup
 $content = select_case 
 get_mac $content
